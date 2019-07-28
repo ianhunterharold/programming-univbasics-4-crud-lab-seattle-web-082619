943edcd5ca0f935141b88a053731cd5efc43dec2
Ian Harold <ianhunterharold@gmail.com>
2019-07-28 20:15:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,9 +9,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  new_array = []
-  new_array << "element"
-  
+ array << "element"
+ # or array.push("element") 
 end
 
 def add_element_to_start_of_array(array, element)
