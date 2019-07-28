72fcb261c89ea80853d78df65f4b153fe1a63447
Ian Harold <ianhunterharold@gmail.com>
2019-07-28 20:12:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -4,7 +4,8 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  created_array = ["sunny","kodi","blitz","Tinkerbell"]
+  p created_array
 end
 
 def add_element_to_end_of_array(array, element)
